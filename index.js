06175dd7d99e4eaf8774e015066c61f9f5c711e2
ViniciusSolique <>
2022-03-09 18:44:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+class Retangulo{
+  constructor( altura, largura){
+    this.altura = altura;
+    this.largura = largura;
+  }
+  calcularArea(){
+    return this.altura*this.largura
+  }
+    area(){
+      return this.calcularArea();
+    }
+  }
+let quadrado = new Retangulo ( "10","10" )
+  let retangulo = new Retangulo ( "30", "60")
+console.log (retangulo.calcularArea())
+console.log (quadrado.area())
+
